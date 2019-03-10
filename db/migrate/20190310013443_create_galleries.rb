b924e55d8625e4e4f40893ec5967745c6c2117db
wilbobraggins <wilbobraggins@gmail.com>
2019-03-10 02:11:58 +0000

    gallery model new sessions and index layout

---
+++ db/migrate/20190310013443_create_galleries.rb
@@ -0,0 +1,9 @@
+class CreateGalleries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :galleries do |t|
+      t.string :name
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_09_222712) do
+ActiveRecord::Schema.define(version: 2019_03_10_013443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "galleries", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
